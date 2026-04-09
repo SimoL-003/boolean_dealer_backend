84a08da283fc0d56dc81e15f2dfc35ea6df62d42
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-04-09 11:54:22 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import "./bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "~resources/scss/app.scss";
+import "~icons/bootstrap-icons.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
